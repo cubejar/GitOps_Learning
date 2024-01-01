ca67482ad923b350042602d792778f3d7c664ba2
cubejar <raj.avudaiappan@gmail.com>
2024-01-01 12:24:18 -0800

    Added function to find 3 comes next to each other

--- python/myacademy/04-Methods-and-Functions/Z-Project-Assignments/2-Function-Find-3-next-to-each-other-on-List.py
+++ python/myacademy/04-Methods-and-Functions/Z-Project-Assignments/2-Function-Find-3-next-to-each-other-on-List.py
@@ -6,3 +6,22 @@ contains a 3 next to a 3 somewhere.
     has_33([3, 1, 3]) → False
 '''
 
+def has_33(my_list):
+    x = 0
+    list_len = len(my_list)
+    mybool = True
+    for x in range(list_len - 1):
+        if my_list[x] == my_list[x + 1] == 3:
+            mybool = True
+            break
+        else:
+            x += 1
+            mybool = False
+            continue
+    print(mybool)
+
+
+has_33([1, 2, 3, 1])
+has_33([1, 5, 2, 3])
+has_33([1, 3, 3])
+has_33([1, 4, 6, 3, 1, 3, 5, 3, 3, 4, 5, 3])
---
+++ python/myacademy/04-Methods-and-Functions/Z-Project-Assignments/3-Function-5-by-5-Representation-Letter.py
@@ -0,0 +1,13 @@
+'''
+#### PRINT BIG: Write a function that takes in a single letter,
+and returns a 5x5 representation of that letter print_big('a')
+out:
+      *
+     * *
+    *****
+    *   *
+    *   *
+HINT: Consider making a dictionary of possible patterns, and mapping
+the alphabet to specific 5 - line combinations of patterns.
+For purposes of this exercise, it's ok if your dictionary stops at "E".
+'''
