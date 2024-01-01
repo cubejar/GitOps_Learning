0cf77bdb1e38e54b846311580f928fec38466abc
cubejar <raj.avudaiappan@gmail.com>
2024-01-01 02:51:30 -0800

    Added few functions - exercise

---
+++ python/myacademy/04-Methods-and-Functions/Z-Project-Assignments/2-Function-Return-a-String-with-3-letters-each.py
@@ -0,0 +1,8 @@
+'''
+#### PAPER DOLL: Given a string,
+return a string where for every character
+in the original there are three characters
+    paper_doll('Hello') --> 'HHHeeellllllooo'
+    paper_doll('Mississippi') --> 'MMMiiissssssiiippppppiii'
+
+'''
